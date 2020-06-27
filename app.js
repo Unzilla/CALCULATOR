71fc469673c65b37865a13ebe0d8069ed0ccaab4
Unzilla <57434325+Unzilla@users.noreply.github.com>
2020-06-27 12:41:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,28 +17,3 @@ function crossResult(){
     var len=res.value.length-1;
     res.value=res.value.substring(0,len);
 }
-function trig(name){
-    if(name==="sin"){
-        var res=document.getElementById('result');
-   res.value=Math.sin();
-
-
-    }else if(name==="cos"){
-        var res=document.getElementById('result');
-   res.value=Math.cos();
-
-
-    }
-    else if(name==="tan"){
-        var res=document.getElementById('result');
-   res.value=Math.tan();
-
-
-    }else if(name==="sq"){
-        var res=document.getElementById('result');
-   res.value=res.value*res.value;
-
-
-    }
-
-}
